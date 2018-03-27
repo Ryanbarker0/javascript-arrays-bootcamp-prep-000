e1b578770f603a2e08e57d6dd026ec30278fbd01
Ryan Barker <ryangeorgebarker0@gmail.com>
2018-03-27 15:35:43 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,44 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+
+function addElementToBeginningOfArray(myArray, myElement) {
+  myArray = [1]
+  myArray.unshift(myElement)
+  return myArray
+}
+
+function destructivelyAddElementToBeginningOfArray(myArray, myElement) {
+  myArray.unshift(myElement)
+  return myArray
+}
+
+function addElementToEndOfArray(myArray, myElement) {
+  myArray = [1]
+  myArray.push(myElement)
+  return myArray
+}
+
+function destructivelyAddElementToEndOfArray(myArray, myElement) {
+  myArray.push(myElement)
+  return myArray
+}
+
+function accessElementInArray(myArray, myIndex) {
+  return myArray[myIndex]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(myArray) {
+  myArray.shift([1])
+  return myArray 
+}
+
+function removeElementFromBeginningOfArray(myArray) {
+  myArray = [1, 2, 3]
+  myArray.slice(1)
+  return myArray
+}
+
+
+
+
+
+
